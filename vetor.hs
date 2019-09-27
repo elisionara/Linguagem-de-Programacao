1dfca3b426b2fdefd58a750876cc004854425b7b
elis <elisionara@alu.ufc.br>
2019-09-27 18:34:25 -0300

    Adding main file

---
+++ vetor.hs
@@ -0,0 +1,9 @@
+{-Função bounds: verificação dos limites do array-}
+
+import Data.Array
+
+{-get_array  :: array (1,4)[(1,'A'),(2, 'B'),(3, 'C'),(4,'D')]-}
+
+--Matriz 2x2
+get_array = array ((1,1),(2,3)) [((1,1),'A',(1,2), 'B', (2,1), 'C', (2,2), 'D')]--tuplas
+ Está dando errado
